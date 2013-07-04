f8651b6323a83f1581e690404c0162f31620b477
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2013-07-03 22:01:10 -0700

    #5 adding Btree topology

--- python/SimEngine/Topology.py
+++ python/SimEngine/Topology.py
@@ -27,6 +27,7 @@ class Topology(object):
     BINARY_TREE    = "BINARY_TREE"
     LINE           = "LINE"
     LATTICE        = "LATTICE"
+    MIN_DISTANCE   = "MIN_DISTANCE"
     
     
     def __new__(cls, *args, **kwargs):
@@ -55,10 +56,12 @@ class Topology(object):
             #make sure that the traffic requirements can be met with that so demanding topology.
             self._createFullMeshTopology()   
         elif type == self.BINARY_TREE:
-            raise NotImplementedError('Mode {0} not implemented'.format(type))
+            self._createBTreeTopology()
         elif type == self.LINE:
             raise NotImplementedError('Mode {0} not implemented'.format(type))         
         elif type == self.LATTICE:
+            raise NotImplementedError('Mode {0} not implemented'.format(type))
+        elif type == self.MIN_DISTANCE:
             raise NotImplementedError('Mode {0} not implemented'.format(type))
         else: 
             raise NotImplementedError('Mode {0} not supported'.format(type))
@@ -86,4 +89,25 @@ class Topology(object):
                         self.motes[nei],
                         s().traffic,
                     )
-        
+                    
+
+    def _addChild(self, id,child):
+        print 'i {0}'.format(id)
+        #downstream link
+        self.motes[id].setDataEngine(self.motes[child], s().traffic)
+        #upstream link
+        self.motes[child].setDataEngine(self.motes[id], s().traffic)
+
+    def _createBTreeTopology(self):
+        print 'len {0}, {1}'.format(len(self.motes),(len(self.motes)/2))
+        for id in range((len(self.motes)/2)):
+              self._addChild(id,(2*id) + 1)
+              print 'child 1 {0}'.format((2*id)+1)
+              #check if it has second child
+              if (2*(id+1)<len(self.motes)):
+                  #other child
+                  self._addChild(id,2*(id+1))
+                  print 'child 2 {0}'.format(2*(id+1))
+            
+            
+            
