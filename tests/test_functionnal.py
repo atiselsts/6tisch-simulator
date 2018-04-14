bc1ed3f84504b5630ddab90078ce9f1e41902270
Thomas Watteyne <twatteyne@gmail.com>
2018-04-15 01:29:36 +0200

    #84. Fixing functional test.

--- tests/test_functionnal.py
+++ tests/test_functionnal.py
@@ -1,38 +1,15 @@
-import sys
-import pytest
 import os
-#from bin import runSim, plot
 import subprocess
 
 # ============================ parameters =====================================
 
-SIM_PARAMETERS = [
-    ['--config', 'bin/config.json']
-]
-PLOT_PARAMETERS = [
-    []
-]
-
 # ============================= fixtures ======================================
-
-@pytest.fixture(params=SIM_PARAMETERS)
-def sim_params(request):
-    return request
-
-@pytest.fixture(params=PLOT_PARAMETERS)
-def plot_params(request):
-    return request
 
 # ============================== tests ========================================
 
-def test_runSim(sim_params):
+def test_runSim():
     wd = os.getcwd()
     os.chdir("bin/")
     rc = subprocess.call("python runSim.py")
     os.chdir(wd)
-    assert rc==100
-
-def test_run_plot(plot_params):
-    sys.argv = [sys.argv[0]] + plot_params.param
-    options = plot.parse_args()
-    plot.main(options)
+    assert rc==0
