b567364475cc894b21d1b3f6d9ff525864a13c0f
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2018-04-20 13:56:55 +0200

    #97 improve test code on singleton by using fixture

--- tests/test_propagation.py
+++ tests/test_propagation.py
@@ -1,58 +1,5 @@
 from SimEngine import Propagation
-from SimEngine import SimSettings
 from SimEngine.Mote import Mote
-
-import pytest
-import time
-
-class TestSingleton:
-
-    @classmethod
-    def setup_method(cls):
-        # make sure a default SimSettings instance
-        settings = SimSettings.SimSettings(**{
-            'exec_numMotes': 1,
-            'exec_simDataDir': "simData",
-            'app_pkPeriod': 0,
-            'rpl_dioPeriod': 0,
-            'rpl_daoPeriod': 0,
-            'sf_type': 'SSF-symmetric',
-            'secjoin_enabled': False,
-            'tsch_slotDuration': 0.010,
-            'tsch_slotframeLength': 101,
-            'tsch_ebPeriod_sec': 0,
-            'top_type': 'linear',
-            'top_squareSide': 2.000,
-            'top_fullyMeshed': False,
-            'prop_type': 'pisterhack',
-            'phy_noInterference': True,
-            'phy_minRssi': -97})
-        start_time = time.strftime("%Y%m%d-%H%M%S")
-        settings.setStartTime(start_time)
-        settings.setCombinationKeys([])
-
-    def teardown_method(cls):
-        # make sure a Propagation instance created during a test is destroyed
-        engine = Propagation.Propagation()
-        engine.destroy()
-
-        settings = SimSettings.SimSettings()
-        settings.destroy()
-
-    def test_instantiate(self):
-        # the second call of Propagation() should return the same instance as the
-        # first call.
-        engine1 = Propagation.Propagation()
-        engine2 = Propagation.Propagation()
-        assert id(engine1) == id(engine2)
-
-    def test_destroy(self):
-        # after calling destroy(), Propagation.Propagation() should return a different (new)
-        # instance to one returned before.
-        engine1 = Propagation.Propagation()
-        engine1.destroy()
-        engine2 = Propagation.Propagation()
-        assert id(engine1) != id(engine2)
 
 
 def test_propagation_from_trace_get_pdr(sim):
