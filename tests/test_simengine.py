59334fd8d25ba95458731e2075ee201df353d7bf
Thomas Watteyne <twatteyne@gmail.com>
2018-04-26 15:17:51 +0200

    #116. dummy SimEngine tests

---
+++ tests/test_simengine.py
@@ -0,0 +1,11 @@
+import time
+
+import pytest
+
+from SimEngine.SimEngine import SimEngine
+
+# test the execution order of events, both per ASN and inside slots
+
+def test_execution_order():
+    
+    pass
