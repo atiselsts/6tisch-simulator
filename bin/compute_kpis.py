e3e8255bcbbddb42199d8e58cc67997e2e08950f
Thomas Watteyne <twatteyne@gmail.com>
2018-05-02 18:15:20 +0200

    #121. basic structure of the KPIs.

--- bin/compute_kpis.py
+++ bin/compute_kpis.py
@@ -1,1 +1,57 @@
-print 'poipoipoi'
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+# =========================== helpers =========================================
+
+def kpi_formation():
+    returnVal = {
+        'formation': 'TODO',
+        # per node?
+    }
+    return returnVal
+
+def kpi_reliability():
+    returnVal = {
+        'reliability': 'TODO',
+        # per node?
+    }
+    return returnVal
+
+def kpi_latency():
+    returnVal = {
+        'latency': 'TODO',
+        # per node?
+        # min/avg/max?
+        # std
+    }
+    return returnVal
+
+def kpi_consumption():
+    returnVal = {
+        'avg': 'TODO',
+        # per node?
+        # per hop?
+        # first death?
+        # last death?
+        # lifetime for different batteries?
+    }
+    return returnVal
+
+# =========================== main ============================================
+
+def all_kpis():
+    kpis = {}
+    kpis['formation']   = kpi_formation()
+    kpis['reliability'] = kpi_reliability()
+    kpis['latency']     = kpi_latency()
+    kpis['consumption'] = kpi_consumption()
+    return kpis
+
+def main():
+    kpis = all_kpis()
+    pp.pprint(kpis)
+    
+if __name__=='__main__':
+    main()
+
