38297da29edb8b9903dbc4bb5361bd1231e8b15f
Keoma Brun <keoma.brun@inria.fr>
2018-05-14 17:03:36 +0200

    fix #155

--- bin/compute_kpis.py
+++ bin/compute_kpis.py
@@ -31,125 +31,124 @@ def openfile(func):
 
 @openfile
 def kpis_all(inputfile):
-    
+
     allstats = {} # indexed by run_id, srcIp
-    
+
     file_settings = json.loads(inputfile.readline())  # first line contains settings
-    
+
     # === gather raw stats
-    
+
     for line in inputfile:
         logline = json.loads(line)
-        
+
         # shorthands
         run_id = logline['_run_id']
-        
+
         # populate
         if run_id not in allstats:
             allstats[run_id] = {}
-        
+
         if   logline['_type'] == SimLog.LOG_TSCH_SYNCED['type']:
             # sync'ed
-            
+
             # shorthands
             mote_id    = logline['_mote_id']
             asn        = logline['_asn']
-            
+
             # only log non-dagRoot sync times
             if mote_id == DAGROOT_ID:
                 continue
-            
+
             # populate
             if mote_id not in allstats[run_id]:
                 allstats[run_id][mote_id] = {}
-            
+
             allstats[run_id][mote_id]['sync_asn']  = asn
             allstats[run_id][mote_id]['sync_time_s'] = asn*file_settings['tsch_slotDuration']
-        
+
         elif logline['_type'] == SimLog.LOG_JOINED['type']:
             # joined
-            
+
             # shorthands
             mote_id    = logline['_mote_id']
             asn        = logline['_asn']
-            
+
             # only log non-dagRoot join times
             if mote_id == DAGROOT_ID:
                 continue
-            
+
             # populate
             assert mote_id in allstats[run_id]
-            
+
             allstats[run_id][mote_id]['join_asn']  = asn
             allstats[run_id][mote_id]['join_time_s'] = asn*file_settings['tsch_slotDuration']
-        
+            allstats[run_id][mote_id]['upstream_pkts'] = {}
+
         elif logline['_type'] == SimLog.LOG_APP_TX['type']:
             # packet transmission
-            
+
             # shorthands
             srcIp      = logline['packet']['net']['srcIp']
             dstIp      = logline['packet']['net']['dstIp']
             appcounter = logline['packet']['app']['appcounter']
             tx_asn     = logline['_asn']
-            
+
             # only log upstream packets
             if dstIp != DAGROOT_IP:
                 continue
-            
+
             # populate
             assert srcIp in allstats[run_id]
-            if 'upstream_pkts' not in allstats[run_id][srcIp]:
-                allstats[run_id][srcIp]['upstream_pkts'] = {}
             if appcounter not in allstats[run_id][srcIp]['upstream_pkts']:
                 allstats[run_id][srcIp]['upstream_pkts'][appcounter] = {
                     'hops': 0,
                 }
-            
+
             allstats[run_id][srcIp]['upstream_pkts'][appcounter]['tx_asn'] = tx_asn
-        
+
         elif logline['_type'] == SimLog.LOG_SIXLOWPAN_PKT_FWD['type']:
             # packet transmission
-            
+
             # shorthands
             pk_type    = logline['packet']['type']
-            
+
             # only consider DATA packets
             if pk_type != 'DATA':
                 continue
-            
+
             srcIp      = logline['packet']['net']['srcIp']
             dstIp      = logline['packet']['net']['dstIp']
             appcounter = logline['packet']['app']['appcounter']
-            
+
             # only consider upstream packets
             if dstIp != DAGROOT_IP:
                 continue
-            
+
             allstats[run_id][srcIp]['upstream_pkts'][appcounter]['hops'] += 1
-        
+
         elif logline['_type'] == SimLog.LOG_APP_RX['type']:
             # packet reception
-            
+
             # shorthands
             srcIp      = logline['packet']['net']['srcIp']
             dstIp      = logline['packet']['net']['dstIp']
             appcounter = logline['packet']['app']['appcounter']
             rx_asn     = logline['_asn']
-            
+
             # only log upstream packets
             if dstIp != DAGROOT_IP:
                 continue
-            
+
             allstats[run_id][srcIp]['upstream_pkts'][appcounter]['hops'] += 1
             allstats[run_id][srcIp]['upstream_pkts'][appcounter]['rx_asn'] = rx_asn
-        
+
         elif logline['_type'] == SimLog.LOG_PACKET_DROPPED['type']:
             # packet dropped
-            
+
             # shorthands
             mote_id    = logline['_mote_id']
             reason     = logline['reason']
-            
+
             # populate
             if mote_id not in allstats[run_id]:
                 allstats[run_id][mote_id] = {}
@@ -157,71 +156,74 @@ def kpis_all(inputfile):
                 allstats[run_id][mote_id]['packet_drops'] = {}
             if reason not in allstats[run_id][mote_id]['packet_drops']:
                 allstats[run_id][mote_id]['packet_drops'][reason] = 0
-            
+
             allstats[run_id][mote_id]['packet_drops'][reason] += 1
-        
+
         elif logline['_type'] == SimLog.LOG_BATT_CHARGE['type']:
             # battery charge
-            
+
             # shorthands
             mote_id    = logline['_mote_id']
             asn        = logline['_asn']
             charge     = logline['charge']
-            
+
             # only log non-dagRoot charge
             if mote_id == DAGROOT_ID:
                 continue
-            
+
             # populate
             if mote_id not in allstats[run_id]:
                 allstats[run_id][mote_id] = {}
             if 'charge' in allstats[run_id][mote_id]:
-                assert charge>=allstats[run_id][mote_id]['charge']
-            
+                assert charge >= allstats[run_id][mote_id]['charge']
+
             allstats[run_id][mote_id]['charge_asn'] = asn
             allstats[run_id][mote_id]['charge']     = charge
-    
+
     # === compute advanced motestats
-    
-    for (run_id,per_mote_stats) in allstats.items():
-        for (srcIp,motestats) in per_mote_stats.items():
+
+    for (run_id, per_mote_stats) in allstats.items():
+        for (srcIp, motestats) in per_mote_stats.items():
             if srcIp != 0:
-            
+
                 if 'sync_asn' in motestats:
                     # ave_current, lifetime_AA
-                    motestats['ave_current_uA'] = float(motestats['charge'])/float( (motestats['charge_asn']-motestats['sync_asn']) * file_settings['tsch_slotDuration'])
-                    motestats['lifetime_AA_years'] = (float(2200*1000)/float(motestats['ave_current_uA']))/(24.0*365)
+                    motestats['ave_current_uA'] = motestats['charge']/float((motestats['charge_asn']-motestats['sync_asn']) * file_settings['tsch_slotDuration'])
+                    motestats['lifetime_AA_years'] = (2200*1000/float(motestats['ave_current_uA']))/(24.0*365)
                 else:
-                    motestats['WARNING'] = 'mote didn\'t sync'
-                
+                    motestats['WARNING'] = "mote didn't sync"
+
                 if 'join_asn' in motestats:
                     # latencies, upstream_num_tx, upstream_num_rx, upstream_num_lost
-                    motestats['latencies'] =  []
-                    motestats['hops']      =  []
-                    motestats['upstream_num_tx'] =     0
-                    motestats['upstream_num_rx'] =     0
-                    motestats['upstream_num_lost'] =   0
-                    for (appcounter,pktstats) in allstats[run_id][srcIp]['upstream_pkts'].items():
+                    motestats['latencies']         = []
+                    motestats['hops']              = []
+                    motestats['upstream_num_tx']   = 0
+                    motestats['upstream_num_rx']   = 0
+                    motestats['upstream_num_lost'] = 0
+                    for (appcounter, pktstats) in allstats[run_id][srcIp]['upstream_pkts'].items():
                         motestats['upstream_num_tx']      += 1
                         if 'rx_asn' in pktstats:
                             motestats['upstream_num_rx']  += 1
                             thislatency = (pktstats['rx_asn']-pktstats['tx_asn'])*file_settings['tsch_slotDuration']
                             motestats['latencies']  += [thislatency]
-                            motestats['hops']       +=  [pktstats['hops']]
+                            motestats['hops']       += [pktstats['hops']]
                         else:
-                            motestats['upstream_num_lost']+= 1
-                    motestats['latency_min_s'] = min(motestats['latencies'])
-                    motestats['latency_avg_s'] = float(sum(motestats['latencies']))/float(len(motestats['latencies']))
-                    motestats['latency_max_s'] = max(motestats['latencies'])
-                    motestats['upstream_reliability'] = float(motestats['upstream_num_rx'])/float(motestats['upstream_num_tx'])
-                    motestats['avg_hops'] = float(sum(motestats['hops']))/float(len(motestats['hops']))
+                            motestats['upstream_num_lost'] += 1
+                    if len(allstats[run_id][srcIp]['upstream_pkts']) > 0:
+                        motestats['latency_min_s'] = min(motestats['latencies'])
+                        motestats['latency_avg_s'] = sum(motestats['latencies'])/float(len(motestats['latencies']))
+                        motestats['latency_max_s'] = max(motestats['latencies'])
+                        motestats['upstream_reliability'] = motestats['upstream_num_rx']/float(motestats['upstream_num_tx'])
+                        motestats['avg_hops'] = sum(motestats['hops'])/float(len(motestats['hops']))
+                    else:
+                        motestats['WARNING'] = "mote didn't send pkts"
                 else:
-                    motestats['WARNING'] = 'mote didn\'t join'
-    
+                    motestats['WARNING'] = "mote didn't join"
+
     # === remove unnecessary stats
-    
-    for (run_id,per_mote_stats) in allstats.items():
-        for (srcIp,motestats) in per_mote_stats.items():
+
+    for (run_id, per_mote_stats) in allstats.items():
+        for (srcIp, motestats) in per_mote_stats.items():
             if 'sync_asn' in motestats:
                 del motestats['sync_asn']
                 del motestats['charge_asn']
@@ -242,7 +244,8 @@ def main():
     # Identify simData having the latest results. That directory should have
     # the latest "mtime".
     subfolders = list(
-        map(lambda x: os.path.join('simData', x),
+        map(
+            lambda x: os.path.join('simData', x),
             os.listdir('simData')
         )
     )
