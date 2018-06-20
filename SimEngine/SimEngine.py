d30e38ac4a6cd240be56da1b340c237dc5a84a87
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2018-06-20 15:50:36 +0200

    SimEngine: add get_mote_by_id()

--- SimEngine/SimEngine.py
+++ SimEngine/SimEngine.py
@@ -196,6 +196,9 @@ class DiscreteEventEngine(threading.Thread):
 
     def getAsn(self):
         return self.asn
+
+    def get_mote_by_id(self, mote_id):
+        return self.motes[mote_id]
     
     #=== scheduling
     
