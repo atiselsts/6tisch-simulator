39f67404f7224949e0f748b67619600eff6fe55a
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2018-07-27 14:58:52 +0200

    #183 remove spaces in blank lines

--- tests/test_rpl.py
+++ tests/test_rpl.py
@@ -17,7 +17,7 @@ def test_ranks_forced_state(sim_engine,fixture_conn_class):
     Verify the force_initial_routing_and_scheduling_state option
     create the expected RPL state.
     '''
-    
+
     sim_engine = sim_engine(
         {
             'exec_numMotes': 3,
@@ -25,7 +25,7 @@ def test_ranks_forced_state(sim_engine,fixture_conn_class):
         },
         force_initial_routing_and_scheduling_state = True
     )
-    
+
     root = sim_engine.motes[0]
     hop1 = sim_engine.motes[1]
     hop2 = sim_engine.motes[2]
@@ -34,12 +34,12 @@ def test_ranks_forced_state(sim_engine,fixture_conn_class):
     assert root.rpl.getPreferredParent()      ==    None
     assert root.rpl.getRank()                 ==     256
     assert root.rpl.getDagRank()              ==       1
-    
+
     assert hop1.dagRoot is False
     assert hop1.rpl.getPreferredParent()      == root.id
     assert hop1.rpl.getRank()                 ==     768
     assert hop1.rpl.getDagRank()              ==       3
-    
+
     if   fixture_conn_class=='FullyMeshed':
         assert hop2.dagRoot is False
         assert hop2.rpl.getPreferredParent()  == root.id
@@ -54,18 +54,18 @@ def test_ranks_forced_state(sim_engine,fixture_conn_class):
         raise SystemError()
 
 def test_source_route_calculation(sim_engine):
-    
+
     sim_engine = sim_engine(
         {
             'exec_numMotes':      1,
         },
     )
-    
+
     mote = sim_engine.motes[0]
-    
+
     # assume DAOs have been receuved by all mote in this topology
     '''          4----5
-                /      
+                /
        0 ----- 1 ------ 2 ----- 3  NODAO  6 ---- 7
     '''
     mote.rpl.addParentChildfromDAOs(parent_id=0, child_id=1)
@@ -75,7 +75,7 @@ def test_source_route_calculation(sim_engine):
     mote.rpl.addParentChildfromDAOs(parent_id=2, child_id=3)
     # no DAO received for 6->3 link
     mote.rpl.addParentChildfromDAOs(parent_id=6, child_id=7)
-    
+
     # verify all source routes
     assert mote.rpl.computeSourceRoute(1) == [1]
     assert mote.rpl.computeSourceRoute(2) == [1,2]
