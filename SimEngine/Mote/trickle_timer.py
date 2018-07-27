e91f07feb696c71b42ea605ce9d3af73a20e27ab
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2018-07-27 17:03:40 +0200

    #183 implement the Trickle Algorithm (RFC 6206)

---
+++ SimEngine/Mote/trickle_timer.py
@@ -0,0 +1,136 @@
+"""
+Trickle Timer: IETF RFC 6206 (https://tools.ietf.org/html/rfc6206)
+"""
+
+import math
+import random
+
+import SimEngine
+import MoteDefines as d
+
+
+class TrickleTimer(object):
+    def __init__(self, i_min, i_max, k, callback):
+        assert isinstance(i_min, (int, long))
+        assert isinstance(i_max, (int, long))
+        assert isinstance(k, (int, long))
+        assert callback is not None
+
+        # shorthand to singletons
+        self.engine   = SimEngine.SimEngine.SimEngine()
+        self.settings = SimEngine.SimSettings.SimSettings()
+
+        # constants of this timer instance
+        # min_interval is expected to given in milliseconds
+        # max_interval is expected to be described as a number of doublings of the
+        # minimum interval size
+        self.min_interval = i_min
+        self.max_interval = self.min_interval * pow(2, i_max)
+        self.redundancy_constant = k
+        self.unique_tag_base = str(id(self))
+
+        # variables
+        self.counter = 0
+        self.interval = 0
+        self.user_callback = callback
+
+    def start(self):
+        # Section 4.2:
+        #   1.  When the algorithm starts execution, it sets I to a value in
+        #       the range of [Imin, Imax] -- that is, greater than or equal to
+        #       Imin and less than or equal to Imax.  The algorithm then begins
+        #       the first interval.
+        self.interval = random.randint(self.min_interval, self.max_interval)
+        self._start_next_interval()
+
+    def stop(self):
+        self.engine.removeFutureEvent(self.unique_tag_base + '_at_i')
+        self.engine.removeFutureEvent(self.unique_tag_base + '_at_t')
+
+    def reset(self):
+        # this method is expected to be called in an event of "inconsistency"
+        #
+        # Section 4.2:
+        #   6.  If Trickle hears a transmission that is "inconsistent" and I is
+        #       greater than Imin, it resets the Trickle timer.  To reset the
+        #       timer, Trickle sets I to Imin and starts a new interval as in
+        #       step 2.  If I is equal to Imin when Trickle hears an
+        #       "inconsistent" transmission, Trickle does nothing.  Trickle can
+        #       also reset its timer in response to external "events".
+        if self.min_interval < self.interval:
+            self.interval = self.min_interval
+            self._start_next_interval()
+        else:
+            # if the interval is equal to the minimum value, do nothing
+            pass
+
+    def increment_counter(self):
+        # this method is expected to be called when a "consistent" transmission
+        # is heard.
+        #
+        # Section 4.2:
+        #   3.  Whenever Trickle hears a transmission that is "consistent", it
+        #       increments the counter c.
+        self.counter += 1
+
+    def _start_next_interval(self):
+        # reset the counter
+        self.counter = 0
+
+        self._schedule_event_at_t()
+        self._schedule_event_at_end_of_interval()
+
+    def _schedule_event_at_t(self):
+        # select t in the range [I/2, I), where I == self.current_interval
+        #
+        # Section 4.2:
+        #   2.  When an interval begins, Trickle resets c to 0 and sets t to a
+        #       random point in the interval, taken from the range [I/2, I),
+        #       that is, values greater than or equal to I/2 and less than I.
+        #       The interval ends at I.
+        slot_len = self.settings.tsch_slotDuration * 1000 # convert to ms
+        t = (1 + random.random()) * self.interval / 2
+        asn = self.engine.getAsn() + int(math.ceil(t / slot_len))
+        if asn == self.engine.getAsn():
+            # schedule the event at the next ASN since we cannot schedule it at
+            # the current ASN
+            asn = self.engine.getAsn() + 1
+
+        def _callback():
+            if self.counter < self.redundancy_constant:
+                #  Section 4.2:
+                #    4.  At time t, Trickle transmits if and only if the
+                #        counter c is less than the redundancy constant k.
+                self.user_callback()
+            else:
+                # do nothing
+                pass
+
+        self.engine.scheduleAtAsn(
+            asn            = asn,
+            cb             = _callback,
+            uniqueTag      = self.unique_tag_base + '_at_t',
+            intraSlotOrder = d.INTRASLOTORDER_STACKTASKS)
+
+    def _schedule_event_at_end_of_interval(self):
+        slot_len = self.settings.tsch_slotDuration * 1000 # convert to ms
+        asn = self.engine.getAsn() + int(math.ceil(self.interval / slot_len))
+
+        def _callback():
+            # doubling the interval
+            #
+            # Section 4.2:
+            #   5.  When the interval I expires, Trickle doubles the interval
+            #       length.  If this new interval length would be longer than
+            #       the time specified by Imax, Trickle sets the interval
+            #       length I to be the time specified by Imax.
+            self.interval = self.interval * 2
+            if self.max_interval < self.interval:
+                self.interval = self.max_interval
+            self._start_next_interval()
+
+        self.engine.scheduleAtAsn(
+            asn            = asn,
+            cb             = _callback,
+            uniqueTag      = self.unique_tag_base + '_at_i',
+            intraSlotOrder = d.INTRASLOTORDER_STACKTASKS)
---
+++ tests/test_trickle_timer.py
@@ -0,0 +1,130 @@
+import pytest
+
+import test_utils as u
+from SimEngine.Mote.trickle_timer import TrickleTimer
+
+# use the default values defined in RFC 6550
+DEFAULT_DIO_INTERVAL_MIN = 3
+DEFAULT_DIO_INTERVAL_DOUBLINGS = 20
+DEFAULT_DIO_REDUNDANCY_CONSTANT = 10
+Imin = pow(2, DEFAULT_DIO_INTERVAL_MIN) # msec
+Imax = DEFAULT_DIO_INTERVAL_DOUBLINGS
+K    = DEFAULT_DIO_REDUNDANCY_CONSTANT
+
+
+def test_initial_state(sim_engine):
+    sim_engine = sim_engine()
+
+    def _callback():
+        pass
+
+    trickle_timer = TrickleTimer(Imin, Imax, K, _callback)
+
+    assert trickle_timer.min_interval == Imin
+    assert trickle_timer.max_interval == Imin * pow(2, Imax)
+    assert trickle_timer.redundancy_constant == K
+    assert trickle_timer.user_callback == _callback
+
+
+@pytest.fixture(params=range(15))
+def num_consistency(request):
+    return request.param
+
+
+def test_redundancy_constant(sim_engine, num_consistency):
+    sim_engine = sim_engine(
+        diff_config = {
+            'exec_numMotes': 1
+        }
+    )
+
+    result = {'is_callback_called': False}
+
+    def _callback():
+        result['is_callback_called'] = True
+
+    trickle_timer = TrickleTimer(Imin, Imax, K, _callback)
+    # set one slotframe long to the interval (for test purpose)
+    INITIAL_INTERVAL = 1010 # ms
+    trickle_timer.interval = INITIAL_INTERVAL
+    trickle_timer._start_next_interval()
+
+    for _ in range(num_consistency):
+        trickle_timer.increment_counter()
+
+    u.run_until_asn(sim_engine, sim_engine.settings.tsch_slotframeLength)
+
+    if num_consistency < K:
+        assert result['is_callback_called'] == True
+    else:
+        assert result['is_callback_called'] == False
+    assert trickle_timer.interval == INITIAL_INTERVAL * 2
+
+
+def test_interval_doubling(sim_engine):
+    sim_engine = sim_engine(
+        diff_config = {
+            'exec_numMotes': 1
+        }
+    )
+
+    def _callback():
+        pass
+
+    one_slotframe = sim_engine.settings.tsch_slotframeLength
+    i_min = 1000
+    i_max = 2
+
+    trickle_timer = TrickleTimer(i_min, i_max, K, _callback)
+    # set one slotframe long to the interval manually (for test purpose)
+    INITIAL_INTERVAL = 1010 # ms
+    trickle_timer.interval = INITIAL_INTERVAL
+    trickle_timer._start_next_interval()
+
+    assert trickle_timer.interval == INITIAL_INTERVAL
+
+    # interval should be doubled
+    u.run_until_asn(sim_engine, sim_engine.getAsn() + one_slotframe)
+    assert trickle_timer.interval == INITIAL_INTERVAL * 2
+
+    # doubled interval will exceed the maximum interval. then, the resulting
+    # interval should be the maximum value
+    u.run_until_asn(sim_engine, sim_engine.getAsn() + one_slotframe * 2)
+    assert trickle_timer.interval == i_min * pow(2, i_max)
+
+
+def test_reset(sim_engine):
+    sim_engine = sim_engine(
+        diff_config = {
+            'exec_numMotes': 1
+        }
+    )
+
+    def _callback():
+        pass
+
+    trickle_timer = TrickleTimer(Imin, Imax, K, _callback)
+    trickle_timer.start()
+
+    # get ASN of 't' and one of the end of the interval
+    original_event_at_t = None
+    original_event_at_end_of_interval = None
+    for event in sim_engine.events:
+        if event[3] == trickle_timer.unique_tag_base + '_at_t':
+            original_event_at_t = event
+        elif event[3] == trickle_timer.unique_tag_base + '_at_i':
+            original_event_at_end_of_interval = event
+
+    u.run_until_asn(sim_engine, sim_engine.getAsn() + 1)
+
+    # reset the timer
+    trickle_timer.reset()
+
+    # interval should be the minimum value by reset()
+    assert trickle_timer.interval == Imin
+    # events should be re-scheduled accordingly
+    for event in sim_engine.events:
+        if event[3] == trickle_timer.unique_tag_base + '_at_t':
+            assert original_event_at_t is not event
+        elif event[3] == trickle_timer.unique_tag_base + '_at_i':
+            assert original_event_at_end_of_interval is not event
